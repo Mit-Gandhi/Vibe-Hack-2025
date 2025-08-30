9c4ee8fc87e0ac8d07180949e6dc707f710c8552
Mit-Gandhi <gandhimit04@gmail.com>
2025-08-31 01:53:52 +0530

    local+global

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,8 @@ if (process.env.NODE_ENV !== 'production') {
       'https://vibe-hack-2025-two.vercel.app',
       'http://localhost:3000',
       'http://localhost:5173',
-      'http://localhost:4173'
+      'http://localhost:4173',
+      'http://localhost:8080'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
